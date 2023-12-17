6c1cc0d4b1de5b6a21b1b5e6186ec69e7e3c5d10
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-12-16 21:57:06 -0300

    first commit

---
+++ src/main/java/com/tpi/TpIntegradorBackCodoACodoApplication.java
@@ -0,0 +1,13 @@
+package com.tpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorBackCodoACodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpIntegradorBackCodoACodoApplication.class, args);
+    }
+
+}
